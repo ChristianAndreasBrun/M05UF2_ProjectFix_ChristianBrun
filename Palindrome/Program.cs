7906498de29d5a2ece224ec6f7379752ed85a092
OtherAKIRA <andreas.christian.brun@gmail.com>
2023-03-27 18:22:25 +0200

    Palindrome - Passed

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -9,19 +9,19 @@ namespace Palindrome
         {
             Console.WriteLine("Please write a text");
             string response = Console.ReadLine();
-            Console.WriteLine(Palindrome(response));
+            Console.WriteLine(Palindrome(response));    //Llama la funcion
         }
         public static string Palindrome(string text)
         {
-            StringBuilder sb = new StringBuilder();
+            StringBuilder sb = new StringBuilder(); //Almacena el texto y su resultado en el orden inverso
 
             sb.Append(text);
-            for (int i = 0; i < text.Length; i++)
-            {
+            for (int i = text.Length - 1; i >= 0; i--)    //"i = t.L -1" -> indice del ultimo char; Si "i >= 0" el bucle se ejecuta; El bucle decrementa "i--" en 1
+            {                                             //Los caracteres del texto, los coloca en orden inverso
                 sb.Append(text[i]);
             }
 
-            return sb.ToString();
+            return sb.ToString();   //Convierte en cadena y la devuelve
         }
     }
 }
